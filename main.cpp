f0a187282e780f0eaad2a00a7cb9a611e9369a28
leiyubiao <1104881327@qq.com>
2021-08-01 11:29:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char** argv) {
     std::cout<<"first time my new branch"<<std::endl;
     std::cout<<"new conflict"<<std::endl;
     return 0;
